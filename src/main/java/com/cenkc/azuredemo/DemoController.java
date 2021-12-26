c3157dfa118a64752ab2e1245a617466808b09f1
CENK CANARSLAN <EXT0208109@turkcell.entp.tgc>
2021-12-26 18:17:11 +0300

    initial commit

---
+++ src/main/java/com/cenkc/azuredemo/DemoController.java
@@ -0,0 +1,16 @@
+package com.cenkc.azuredemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * created by Cenk Canarslan on 2021-12-26
+ */
+@RestController
+public class DemoController {
+
+    @GetMapping("/")
+    public String demoEndPoint() {
+        return "Greetings from Spring Boot";
+    }
+}
